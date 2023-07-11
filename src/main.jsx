7bec52a68232513f7b94707e0ca8b4109e6119b9
Hannah-Liao <hanna.liao@outlook.com>
2023-07-11 21:16:25 +0100

    feat: add mui theme customisation

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+
+import theme from './theme';
 import { App } from './app.jsx';
 import './index.css';
 
@@ -7,7 +10,9 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
+   <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById('app')
 );
