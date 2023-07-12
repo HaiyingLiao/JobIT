740b81ff4937c48f4492232d5be4b66b93e60043
surajarya130 <surajarya130@gmail.com>
2023-07-12 17:38:28 +0530

    #1 eslint setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./app";
-import "./index.css";
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app';
+import './index.css';
 
-const root = createRoot(document.getElementById("root"));
-let ele = "Suraj";
+const root = createRoot(document.getElementById('root'));
+let ele = 'Suraj';
 console.log(ele);
 
 root.render(
