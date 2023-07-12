15aa00c3beff87584098a20301e126cf5c5482da
surajarya130 <surajarya130@gmail.com>
2023-07-12 17:00:53 +0530

    #1 eslint setup

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,19 @@
+import React from 'react';
 import './app.css';
 import { Routes, Route } from 'react-router-dom';
 
 import { Navbar } from './components';
 import { Home, JobSearch, EstimatedSalary } from './pages';
 
-export function App() {
+export default function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/job-search" element={<JobSearch />} />
-        <Route exact path="/estimated-salary" element={<EstimatedSalary />} />
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/job-search' element={<JobSearch />} />
+        <Route exact path='/estimated-salary' element={<EstimatedSalary />} />
       </Routes>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { App } from './app.jsx';
-import './index.css';
+import React from "react";
 
-const root = createRoot(document.getElementById('root'));
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app";
+import "./index.css";
+
+const root = createRoot(document.getElementById("root"));
+let ele = "Suraj";
+console.log(ele);
 
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('app')
 );
