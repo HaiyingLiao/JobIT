e737c34d3cdd090c940c214ecc31e41a00ad9489
Mateo Sindičić <mateo.sindicic@gmail.com>
2023-07-17 14:47:41 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
 
 import theme from './theme';
 import { App } from './app.jsx';
@@ -11,8 +10,6 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-   <ThemeProvider theme={theme}>
     <App />
-    </ThemeProvider>
   </BrowserRouter>,
 );
