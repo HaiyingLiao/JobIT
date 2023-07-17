c0db3c127535f8e36446268825c31eb61bbae514
wirayuda <wirayuda233@gmail.com>
2023-07-17 22:57:42 +0800

    add and setup react-hot-toast

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 // install es7 snippets extension for VS Code https://marketplace.visualstudio.com/items?itemName=dsznajder.es7-react-js-snippets
 // type rafce and press Enter
 import React from 'react';
+import { toast } from 'react-hot-toast';
 
 const Home = () => {
 	return <div>Home/Overview Page</div>;
