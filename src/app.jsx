c76f8e096f19fe544438ebb67566d53725821f31
Hannah-Liao <hanna.liao@outlook.com>
2023-07-18 18:21:02 +0100

    update theme setting

--- src/app.jsx
+++ src/app.jsx
@@ -1,37 +1,36 @@
-import React, { useState } from "react";
-import "./app.css";
-import { Routes, Route } from "react-router-dom";
-// import Switch from '@mui/material/Switch';
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from 'react';
+import './app.css';
+import { Routes, Route } from 'react-router-dom';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 
-import { InputValidation, Navbar, SwitchButton } from "./components";
-import { Home, JobSearch, EstimatedSalary } from "./pages";
+import { InputValidation, Navbar, SwitchButton } from './components';
+import { Home, JobSearch, EstimatedSalary } from './pages';
 
 export default function App() {
   const [darkMode, setDarkMode] = useState(false);
-  const themeSec = createTheme({
-    palette: {
-      mode: darkMode ? "dark" : "light",
-    },
-  });
+  // const themeSec = createTheme({
+  //   palette: {
+  //     mode: darkMode ? "dark" : "light",
+  //   },
+  // });
 
   const handleChange = () => {
     setDarkMode((prevMode) => !prevMode);
   };
 
   return (
-    <ThemeProvider theme={themeSec}>
-      <>
-        <CssBaseline />
-        <SwitchButton checkMode={darkMode} changeMode={handleChange} />
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/job-search" element={<JobSearch />} />
-          <Route exact path="/estimated-salary" element={<EstimatedSalary />} />
-        </Routes>
-        <InputValidation />
-      </>
-    </ThemeProvider>
+    // <ThemeProvider theme={themeSec}>
+    <>
+      <CssBaseline />
+      <SwitchButton checkMode={darkMode} changeMode={handleChange} />
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/job-search' element={<JobSearch />} />
+        <Route exact path='/estimated-salary' element={<EstimatedSalary />} />
+      </Routes>
+      <InputValidation />
+    </>
+    // </ThemeProvider>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
 
 import theme from "./theme";
 import App from "./app.jsx";
