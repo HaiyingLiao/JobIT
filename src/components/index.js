a3cf9217fb16135ff1658369806d2fd2475d89d4
wirayuda <wirayuda233@gmail.com>
2023-07-19 15:30:18 +0800

    remove export JobCard that doesnt exits

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,3 @@ export { default as InputValidation } from './InputValidation/InputValidation';
 export { default as SwitchButton } from './SwitchButton/SwitchButton';
 export { default as NotFound } from './Error/NotFound/NotFound';
 export { default as ServerError } from './Error/ServerError/ServerError';
-export { default as JobCard } from './Card/JobCard';
