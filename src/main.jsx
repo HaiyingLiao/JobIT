4b11e4f231307c0bb00107f6fe766fd96d9d66a8
Hannah-Liao <hanna.liao@outlook.com>
2023-07-19 11:18:32 +0100

    refactor code for light mode and dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@emotion/react';
 import { Toaster } from 'react-hot-toast';
 
-import theme from './theme';
 import App from './app.jsx';
 import './index.css';
 
@@ -11,9 +9,7 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
-    <ThemeProvider theme={theme}>
-      <App />
-      <Toaster />
-    </ThemeProvider>
+    <App />
+    <Toaster />
   </BrowserRouter>
 );
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,120 @@
 import { createTheme } from '@mui/material/styles';
 
-// A custom theme for this app
-const theme = createTheme({
+const typography = {
+  fontFamily: "'Manrope', sans-serif",
+  h1: {
+    fontSize: '2rem',
+    fontWeight: 700,
+  },
+  h2: {
+    fontSize: '1.5rem',
+    fontWeight: 700,
+  },
+  h3: {
+    fontSize: '1.5rem',
+    fontWeight: 600,
+  },
+  h4: {
+    fontSize: '1.375rem',
+    fontWeight: 700,
+  },
+  h5: {
+    fontSize: '1.375rem',
+    fontWeight: 600,
+  },
+  // 18px
+  bodyL: {
+    fontSize: '1.125rem',
+    fontWeight: 700,
+  },
+  bodyL_2: {
+    fontSize: '1.125rem',
+    fontWeight: 600,
+  },
+  bodyL_3: {
+    fontSize: '1.125rem',
+    fontWeight: 500,
+  },
+  bodyL_4: {
+    fontSize: '1.125rem',
+    fontWeight: 400,
+  },
+  // 16px
+  bodyM: {
+    fontSize: '1rem',
+    fontWeight: 700,
+  },
+  bodyM_2: {
+    fontSize: '1rem',
+    fontWeight: 600,
+  },
+  bodyM_3: {
+    fontSize: '1rem',
+    fontWeight: 500,
+  },
+  bodyM_4: {
+    fontSize: '1rem',
+    fontWeight: 400,
+  },
+  // 15px
+  bodyM2: {
+    fontSize: '0.9375rem',
+    fontWeight: 700,
+  },
+  bodyM2_2: {
+    fontSize: '0.9375rem',
+    fontWeight: 600,
+  },
+  bodyM2_3: {
+    fontSize: '0.9375rem',
+    fontWeight: 500,
+  },
+  bodyM2_4: {
+    fontSize: '0.9375rem',
+    fontWeight: 400,
+  },
+  // 14px
+  bodyM3: {
+    fontSize: '0.875rem',
+    fontWeight: 700,
+  },
+  bodyM3_2: {
+    fontSize: '0.875rem',
+    fontWeight: 600,
+  },
+  bodyM3_3: {
+    fontSize: '0.875rem',
+    fontWeight: 500,
+  },
+  bodyM3_4: {
+    fontSize: '0.875rem',
+    fontWeight: 400,
+  },
+  // 13px
+  bodyM4: {
+    fontSize: '0.8125rem',
+    fontWeight: 700,
+  },
+  bodyM4_2: {
+    fontSize: '0.8125rem',
+    fontWeight: 600,
+  },
+  bodyM4_3: {
+    fontSize: '0.8125rem',
+    fontWeight: 500,
+  },
+  bodyM4_4: {
+    fontSize: '0.8125rem',
+    fontWeight: 400,
+  },
+  subtitle1: undefined,
+  subtitle2: undefined,
+  body1: undefined,
+  body2: undefined,
+  caption: undefined,
+};
+
+export const lightTheme = createTheme({
   palette: {
     primary: {
       main: '#0BAB7C',
@@ -24,118 +137,8 @@ const theme = createTheme({
     },
   },
   typography: {
-    fontFamily: "'Manrope', sans-serif",
-    h1: {
-      fontSize: '2rem',
-      fontWeight: 700,
-    },
-    h2: {
-      fontSize: '1.5rem',
-      fontWeight: 700,
-    },
-    h3: {
-      fontSize: '1.5rem',
-      fontWeight: 600,
-    },
-    h4: {
-      fontSize: '1.375rem',
-      fontWeight: 700,
-    },
-    h5: {
-      fontSize: '1.375rem',
-      fontWeight: 600,
-    },
-    // 18px
-    bodyL: {
-      fontSize: '1.125rem',
-      fontWeight: 700,
-    },
-    bodyL_2: {
-      fontSize: '1.125rem',
-      fontWeight: 600,
-    },
-    bodyL_3: {
-      fontSize: '1.125rem',
-      fontWeight: 500,
-    },
-    bodyL_4: {
-      fontSize: '1.125rem',
-      fontWeight: 400,
-    },
-    // 16px
-    bodyM: {
-      fontSize: '1rem',
-      fontWeight: 700,
-    },
-    bodyM_2: {
-      fontSize: '1rem',
-      fontWeight: 600,
-    },
-    bodyM_3: {
-      fontSize: '1rem',
-      fontWeight: 500,
-    },
-    bodyM_4: {
-      fontSize: '1rem',
-      fontWeight: 400,
-    },
-    // 15px
-    bodyM2: {
-      fontSize: '0.9375rem',
-      fontWeight: 700,
-    },
-    bodyM2_2: {
-      fontSize: '0.9375rem',
-      fontWeight: 600,
-    },
-    bodyM2_3: {
-      fontSize: '0.9375rem',
-      fontWeight: 500,
-    },
-    bodyM2_4: {
-      fontSize: '0.9375rem',
-      fontWeight: 400,
-    },
-    // 14px
-    bodyM3: {
-      fontSize: '0.875rem',
-      fontWeight: 700,
-    },
-    bodyM3_2: {
-      fontSize: '0.875rem',
-      fontWeight: 600,
-    },
-    bodyM3_3: {
-      fontSize: '0.875rem',
-      fontWeight: 500,
-    },
-    bodyM3_4: {
-      fontSize: '0.875rem',
-      fontWeight: 400,
-    },
-    // 13px
-    bodyM4: {
-      fontSize: '0.8125rem',
-      fontWeight: 700,
-    },
-    bodyM4_2: {
-      fontSize: '0.8125rem',
-      fontWeight: 600,
-    },
-    bodyM4_3: {
-      fontSize: '0.8125rem',
-      fontWeight: 500,
-    },
-    bodyM4_4: {
-      fontSize: '0.8125rem',
-      fontWeight: 400,
-    },
-    subtitle1: undefined,
-    subtitle2: undefined,
-    body1: undefined,
-    body2: undefined,
-    caption: undefined,
+    ...typography,
   },
 });
 
-export default theme;
+export const darkTheme = createTheme({});
