2308e370370d75e202e8a0f3be723a84205dbde2
surajarya130 <surajarya130@gmail.com>
2023-07-19 14:17:54 +0530

    Added .vscode and eslint file setup

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,43 @@
-import { useState } from 'react';
 import './app.css';
 import { Routes, Route } from 'react-router-dom';
-import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+import {
+  CssBaseline,
+  // createTheme,
+  // ThemeProvider
+} from '@mui/material';
 
-import { InputValidation, Navbar, SwitchButton } from './components';
+import {
+  InputValidation,
+  Navbar,
+  // SwitchButton
+} from './components';
 import { Home, JobSearch, EstimatedSalary } from './pages';
 
 export default function App() {
-  const [darkMode, setDarkMode] = useState(false);
-  const themeSec = createTheme({
-    palette: {
-      mode: darkMode ? 'dark' : 'light',
-    },
-  });
+  // const [darkMode, setDarkMode] = useState(false);
+  // const themeSec = createTheme({
+  //   palette: {
+  //     mode: darkMode ? 'dark' : 'light',
+  //   },
+  // });
 
-  const handleChange = () => {
-    setDarkMode((prevMode) => !prevMode);
-  };
+  // const handleChange = () => {
+  //   setDarkMode((prevMode) => !prevMode);
+  // };
 
   return (
-    <ThemeProvider theme={themeSec}>
-      <>
-        <CssBaseline />
-        <SwitchButton checkMode={darkMode} changeMode={handleChange} />
-        <Navbar />
-        <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/job-search' element={<JobSearch />} />
-          <Route exact path='/estimated-salary' element={<EstimatedSalary />} />
-        </Routes>
-        <InputValidation />
-      </>
-    </ThemeProvider>
+    // <ThemeProvider theme={themeSec}>
+    <>
+      <CssBaseline />
+      {/* <SwitchButton checkMode={darkMode} changeMode={handleChange} /> */}
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/job-search' element={<JobSearch />} />
+        <Route exact path='/estimated-salary' element={<EstimatedSalary />} />
+      </Routes>
+      <InputValidation />
+    </>
+    // </ThemeProvider>
   );
 }
