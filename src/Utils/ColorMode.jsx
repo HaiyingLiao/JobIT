ae5a3d058e26b452fee9a4e545aba75d4d3cb972
surajarya130 <surajarya130@gmail.com>
2023-07-20 07:04:19 +0530

    test

--- src/Utils/ColorMode.jsx
+++ src/Utils/ColorMode.jsx
@@ -1,34 +1,3 @@
-<<<<<<< Updated upstream
 import { createContext } from 'react';
 
 export const ColorModeContext = createContext({});
-=======
-import { useState, createContext, useMemo } from "react";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
-
-export const ColorModeContext = createContext();
-
-const ColorMode = ({ children }) => {
-  const [mode, setMode] = useState("light");
-  const toggleColorMode = () => {
-    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-  };
-  const theme = useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode],
-  );
-
-  return (
-    <ColorModeContext.Provider value={{ mode, setMode, toggleColorMode }}>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </ColorModeContext.Provider>
-  );
-};
-
-export default ColorMode;
->>>>>>> Stashed changes
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -13,20 +12,5 @@ root.render(
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-=======
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-
-import App from "./app.jsx";
-import "./index.css";
-
-const root = createRoot(document.getElementById("root"));
-
-root.render(
-  <BrowserRouter>
-    <App />
-    <Toaster />
->>>>>>> Stashed changes
   </BrowserRouter>,
 );
