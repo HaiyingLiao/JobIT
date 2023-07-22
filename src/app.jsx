75544a77b3bfcde61864258fa934a7d4ee988bc0
surajarya130 <surajarya130@gmail.com>
2023-07-22 15:39:39 +0530

    #1 home page

--- src/app.jsx
+++ src/app.jsx
@@ -2,23 +2,28 @@
 import { Routes, Route } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
 
-import { InputValidation, Navbar } from './components';
+import { Navbar } from './components';
 
 import { Home, JobSearch, EstimatedSalary } from './pages';
 
 import './app.css';
+import { Box } from '@mui/system';
 
 export default function App() {
   return (
     <>
-      <CssBaseline />
-      <Navbar />
-      <Routes>
-        <Route exact path='/' element={<Home />} />
-        <Route exact path='/job-search' element={<JobSearch />} />
-        <Route exact path='/estimated-salary' element={<EstimatedSalary />} />
-      </Routes>
-      <InputValidation />
+      <Box
+        className='mainContainer'
+        sx={{ display: 'flex', height: '100%', backgroundColor: '#f9fafc' }}
+      >
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/job-search' element={<JobSearch />} />
+          <Route exact path='/estimated-salary' element={<EstimatedSalary />} />
+        </Routes>
+      </Box>
     </>
   );
 }
