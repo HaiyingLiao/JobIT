3eeb2ab90e425b2ed2930f27fba22e89dd3f7597
Hayley <hayleyiscoding@gmail.com>
2023-07-29 02:04:38 +0530

    create input form and chart

---
+++ src/pages/EstimatedSalary/Chart.jsx
@@ -0,0 +1,93 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { Box } from '@mui/material';
+import { useTheme } from '@emotion/react';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip);
+
+const getOptions = (isDark) => ({
+  indexAxis: 'x',
+  elements: {
+    bar: {
+      borderWidth: 2,
+      borderColor: 'rgba(0, 0, 0, 0)',
+    },
+  },
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {},
+  scales: {
+    y: {
+      beginAtZero: false,
+      ticks: {
+        callback: function (value) {
+          if (!value) return;
+          return `${value} USD`;
+        },
+        stepSize: 20000,
+      },
+      grid: {
+        color: isDark ? '#1c1c24' : '#F5F5F8',
+      },
+    },
+
+    x: {
+      ticks: {
+        font: {
+          size: 14,
+        },
+      },
+      grid: {
+        display: false,
+      },
+    },
+  },
+});
+
+const data = {
+  labels: ['Glassdoor', 'Indeed', 'Salary.com'],
+  datasets: [
+    {
+      data: [40000, 30000, 50000],
+      backgroundColor: '#FDDD8C',
+      barThickness: 12,
+      borderRadius: 50,
+    },
+    {
+      data: [10000, 60000, 30000],
+      backgroundColor: '#0BAB7C',
+      barThickness: 12,
+      borderRadius: 50,
+    },
+    {
+      data: [20000, 50000, 60000],
+      backgroundColor: '#FFBBD7',
+      barThickness: 12,
+      borderRadius: 50,
+    },
+  ],
+};
+
+export default function Chart() {
+  const isDarkTheme = useTheme().palette.mode === 'dark';
+
+  return (
+    <Box
+      sx={{
+        width: { sm: '100%', lg: '80%' },
+        margin: '0 auto',
+        height: '250px',
+        paddingTop: '30px',
+      }}
+    >
+      <Bar options={getOptions(isDarkTheme)} data={data} />
+    </Box>
+  );
+}
--- src/pages/EstimatedSalary/EstimatedSalary.jsx
+++ src/pages/EstimatedSalary/EstimatedSalary.jsx
@@ -1,5 +1,206 @@
+import {
+  Grid,
+  Typography,
+  TextField,
+  Box,
+  ListItem,
+  useTheme,
+  useMediaQuery,
+} from '@mui/material';
+import Chart from './Chart';
+
 const EstimatedSalary = () => {
-  return <div>EstimatedSalary</div>;
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  return (
+    <Box
+      sx={{
+        backgroundColor: 'customColor.pageBG',
+        minHeight: '100vh',
+      }}
+    >
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          padding: { xs: '120px 24px 0 24px', md: '120px 80px 0 80px' },
+        }}
+      >
+        <Grid item sm={6} xs={12}>
+          <Typography variant={isMobile ? 'h4' : 'h1'} mb='12px'>
+            Estimated Salaries
+          </Typography>
+          <Typography
+            variant={isMobile ? 'bodyM_3' : 'h6'}
+            color='text.secondary'
+          >
+            Monday, 13 Jan 2023
+          </Typography>
+          <Grid
+            container
+            spacing={2}
+            sx={{ py: '38px', pr: { sm: '20px', lg: '85px' } }}
+          >
+            <Grid item xs={12}>
+              <Typography
+                variant={isMobile ? 'bodyM3_2' : 'bodyM2_2'}
+                color='secondary.contrastText'
+                sx={{ lineHeight: '24px' }}
+              >
+                Job Title
+              </Typography>
+              <TextField
+                fullWidth
+                placeholder='Senior User Experience Designer'
+                sx={{
+                  borderRadius: '12px',
+                  mt: '12px',
+                  bgcolor: 'customColor.toggleBtn',
+                  border: '1px solid rgba(226, 226, 234, 0.60)',
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography
+                variant={isMobile ? 'bodyM3_2' : 'bodyM2_2'}
+                color='secondary.contrastText'
+                sx={{ lineHeight: '24px' }}
+              >
+                Location
+              </Typography>
+              <TextField
+                fullWidth
+                placeholder='New-York, NY, USA'
+                sx={{
+                  borderRadius: '12px',
+                  bgcolor: 'customColor.toggleBtn',
+                  mt: '12px',
+                  border: '1px solid rgba(226, 226, 234, 0.60)',
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography
+                variant={isMobile ? 'bodyM3_2' : 'bodyM2_2'}
+                color='secondary.contrastText'
+                sx={{ lineHeight: '24px' }}
+              >
+                Radius
+              </Typography>
+              <TextField
+                fullWidth
+                placeholder='100'
+                sx={{
+                  borderRadius: '12px',
+                  mt: '12px',
+                  bgcolor: 'customColor.toggleBtn',
+                  border: '1px solid rgba(226, 226, 234, 0.60)',
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          sm={6}
+          xs={12}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItemst: 'flex-start',
+            bgcolor: 'customColor.jobCardBg',
+            borderRadius: '10px',
+            padding: '26px',
+            marginTop: isMobile ? '0' : '45px',
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: { sm: '1rem', md: '1.5rem' },
+              fontWeight: '400',
+              paddingLeft: isMobile ? '0' : '26px',
+            }}
+            mb='8px'
+          >
+            <Typography component='span' variant={isMobile ? 'bodyM' : 'h2'}>
+              Estimated Salary
+            </Typography>
+            &nbsp;for&nbsp;
+            <Typography component='span' variant={isMobile ? 'bodyM' : 'h2'}>
+              NodeJS Developer
+            </Typography>
+            &nbsp;in&nbsp;
+            <Typography component='span' variant={isMobile ? 'bodyM' : 'h2'}>
+              New York
+            </Typography>
+          </Typography>
+          <Box display='flex' paddingLeft={isMobile ? '0' : '26px'}>
+            <ListItem
+              disablePadding
+              sx={{ paddingRight: isMobile ? '6px' : '0' }}
+            >
+              <Box
+                sx={{
+                  borderRadius: '60px',
+                  bgcolor: '#FDDD8C',
+                  height: '10px',
+                  width: '10px',
+                  marginRight: isMobile ? '4px' : '8px',
+                }}
+              ></Box>
+              <Typography
+                variant={!isMobile && 'bodyM4_3'}
+                sx={{ fontSize: isMobile ? '10px' : '13px' }}
+              >
+                Minimum Salary
+              </Typography>
+            </ListItem>
+            <ListItem
+              disablePadding
+              sx={{ paddingRight: isMobile ? '6px' : '0' }}
+            >
+              <Box
+                sx={{
+                  borderRadius: '60px',
+                  bgcolor: '#0BAB7C',
+                  height: '10px',
+                  width: '10px',
+                  marginRight: isMobile ? '4px' : '8px',
+                }}
+              ></Box>
+              <Typography
+                variant={!isMobile && 'bodyM4_3'}
+                sx={{ fontSize: isMobile ? '10px' : '13px' }}
+              >
+                Maximum Salary
+              </Typography>
+            </ListItem>
+            <ListItem
+              disablePadding
+              sx={{ paddingRight: isMobile ? '6px' : '0' }}
+            >
+              <Box
+                sx={{
+                  borderRadius: '60px',
+                  bgcolor: '#FFBBD7',
+                  height: '10px',
+                  width: '10px',
+                  marginRight: isMobile ? '4px' : '8px',
+                }}
+              ></Box>
+              <Typography
+                variant={!isMobile && 'bodyM4_3'}
+                sx={{ fontSize: isMobile ? '10px' : '13px' }}
+              >
+                Median Salary
+              </Typography>
+            </ListItem>
+          </Box>
+          <Chart />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default EstimatedSalary;
