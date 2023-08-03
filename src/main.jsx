3dc7d1440d8c6ff5a51155ea3e13061f088986f1
wirayuda <wirayuda233@gmail.com>
2023-08-03 20:38:06 +0800

    fix duplicate import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ColorModeProvider from './Utils/ColorMode';
-import { Provider } from 'react-redux';
 
 import App from './app.jsx';
 import './index.css';
